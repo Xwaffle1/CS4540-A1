0348cc133538f6e4af8322a0391e596130e06b75
Chase Myers <chase.a.myers@wmich.edu>
2019-01-17 19:01:29 -0500

    Reading file into memory.

--- readfile.c
+++ readfile.c
@@ -2,4 +2,9 @@
 #include "structs.h"
 void readFile(zipTowns arrs, FILE * infile, int * length){
   printf("%s\n", "Read File called.");
+  char buffer[255];
+  while(fgets(buffer, 255, infile)) {
+    printf("%s", buffer);
+  }
+
 }
